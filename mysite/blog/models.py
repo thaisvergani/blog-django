002def917b95d23881543705da17014953adf186
Thaís Vergani <thais.vergani1@gmail.com>
2022-07-09 11:18:57 -0300

    remove comment

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
