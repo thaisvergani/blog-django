8427560d45c05f3c5012280e0350c523e60f0cea
Thaís Vergani <thais.vergani1@gmail.com>
2022-07-23 09:24:02 -0300

    add comment model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,3 +16,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
